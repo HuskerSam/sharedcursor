abb689f259b309d1d382a6b43030776356cc74e2
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2022-12-05 18:13:30 -0600

    mesh size to fit code

--- public/models/storyapp.js
+++ public/models/storyapp.js
@@ -837,6 +837,15 @@ export class StoryApp extends BaseApp {
     this.selectedMeshInstance = await this.__loadRotatingAsset(assetMeta);
     this.selectedMeshInstance.wrapper.position.y = 2.5;
     this.selectedMeshInstance.wrapper.parent = this.playerLeftPanelTransform;
+
+    this._fitNodeToSize(this.selectedMeshInstance.wrapper, 2.5);
+  }
+  _fitNodeToSize(node, size) {
+    const boundingInfo = node.getHierarchyBoundingVectors(true);
+    const currentLength = boundingInfo.max.subtract(boundingInfo.min);
+    const biggestSide = Math.max(currentLength.x, Math.max(currentLength.y, currentLength.z));
+    let scale = size / biggestSide;
+    node.scaling.scaleInPlace(scale);
   }
   clickEndTurn() {
     this._endTurn();
@@ -1642,66 +1651,8 @@ export class StoryApp extends BaseApp {
       return;
     }
   }
-  createGuides(size = 50) {
-    let wrapper = null;
-    let sObjects = [];
-    let axisX = BABYLON.Mesh.CreateLines("axisX", [
-      new BABYLON.Vector3.Zero(),
-      new BABYLON.Vector3(size, 0, 0),
-      new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
-      new BABYLON.Vector3(size, 0, 0),
-      new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
-    ], this.scene);
-    axisX.color = new BABYLON.Color3(1, 0, 0);
-    wrapper = axisX;
-
-    let localScene = this.scene;
-
-    function __make2DTextMesh(text, color, size) {
-      let dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, localScene, true);
-      dynamicTexture.hasAlpha = true;
-      dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
-      let plane = new BABYLON.Mesh.CreatePlane("TextPlane", size, localScene, true);
-      plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", localScene);
-      plane.material.backFaceCulling = false;
-      plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
-      plane.material.diffuseTexture = dynamicTexture;
-      return plane;
-    }
-
-    let xChar = __make2DTextMesh("X", "red", size / 10);
-    xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
-    xChar.setParent(wrapper);
-
-    let axisY = BABYLON.Mesh.CreateLines("axisY", [
-      new BABYLON.Vector3.Zero(),
-      new BABYLON.Vector3(0, size, 0),
-      new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
-      new BABYLON.Vector3(0, size, 0),
-      new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
-    ], this.scene);
-    axisY.color = new BABYLON.Color3(0, 1, 0);
-    axisY.setParent(wrapper);
-
-    let yChar = __make2DTextMesh("Y", "green", size / 10);
-    yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
-    yChar.setParent(wrapper);
-
-    let axisZ = BABYLON.Mesh.CreateLines("axisZ", [
-      new BABYLON.Vector3.Zero(),
-      new BABYLON.Vector3(0, 0, size),
-      new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
-      new BABYLON.Vector3(0, 0, size),
-      new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
-    ], this.scene);
-    axisZ.color = new BABYLON.Color3(0, 0, 1);
-    axisZ.setParent(wrapper);
-
-    let zChar = __make2DTextMesh("Z", "blue", size / 10);
-    zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
-    zChar.setParent(wrapper);
-
-    return wrapper;
+  createGuides(size = 30) {
+    return new BABYLON.AxesViewer(this.scene, size);
   }
 
   updateScoreboard() {
@@ -1833,6 +1784,8 @@ export class StoryApp extends BaseApp {
     this.selectedMeshInstance.wrapper.position.y = 2.5;
 
     this.selectedMeshInstance.wrapper.parent = this.playerLeftPanelTransform;
+
+    this._fitNodeToSize(this.selectedMeshInstance.wrapper, 2.5);
 
     this.activeMoonNav = Utility3D.__createTextMesh('activemoonnavigate', {
       text: 'A Follow',
